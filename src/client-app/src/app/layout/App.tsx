c69cb582dbf0b5e61ceb33e702fae63fac5d3f42
Szymon Oles <szyoles@gmail.com>
2021-12-04 01:21:42 +0100

    53 done.

--- src/client-app/src/app/layout/App.tsx
+++ src/client-app/src/app/layout/App.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import axios from "axios";
-import { Header, List } from "semantic-ui-react";
+import { Container, Header, List } from "semantic-ui-react";
 import { Activity } from "../models/activity";
 import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -17,19 +18,12 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <NavBar/>
-      
-      <List>
-        {activities.map((activity) => (
-          <List.Item key={activity.id}>
-            {activity.title}
-            </List.Item>
-        ))}
-      </List>
-
-      <ul></ul>
-    </div>
+    <>
+      <NavBar />
+      <Container style={{ marginTop: "7em" }}>
+        <ActivityDashboard activities={activities} />
+      </Container>
+    </>
   );
 }
 
