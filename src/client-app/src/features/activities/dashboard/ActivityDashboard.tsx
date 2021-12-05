fe8db49248b544f306045212f73c958ebedd5b24
Szymon Oles <szyoles@gmail.com>
2021-12-05 01:11:07 +0100

    60. done

--- src/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ src/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, List } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
@@ -7,19 +7,36 @@ import ActivityList from "./ActivityList";
 
 interface Props {
   activities: Activity[];
+  selectedActivity: Activity | undefined;
+  selectActivity: (id: string) => void;
+  cancelSelectActivity: () => void;
+  editMode: boolean;
+  openForm: (id: string) => void;
+  closeForm: () => void;
+  createOrEdit: (activity: Activity) => void;
+  deleteActivity: (id: string) => void;
 }
 
-export default function ActivityDashboard({ activities }: Props) {
+export default function ActivityDashboard({ activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity }: Props) {
   return (
     <Grid>
       <Grid.Column width="10">
-        <ActivityList activities={activities} />
+        <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity} />
       </Grid.Column>
       <Grid.Column width="6">
-        {activities[0] && ( // ActivityDetails only of acitivites[0] != null
-          <ActivityDetails activity={activities[0]} />
+        {selectedActivity && !editMode && ( // ActivityDetails only of acitivites[0] != null
+          <ActivityDetails 
+          activity={selectedActivity} 
+          cancelSelectActivity={cancelSelectActivity}
+          openForm={openForm}
+          />
         )}
-        <ActivityForm />
+        {editMode &&
+        <ActivityForm 
+          closeForm={closeForm}
+          activity={selectedActivity}
+          createOrEdit={createOrEdit}
+          />}
       </Grid.Column>
     </Grid>
   );
